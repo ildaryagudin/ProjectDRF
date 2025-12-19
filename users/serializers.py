ab637db53d352c683059fd3b207cafba785fdf6a
ildaryagudin <ilyagudin70@gmail.com>
2025-12-19 23:24:00 +0500

    32 done

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,40 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import User, Payment
-from materials.serializers import CourseSerializer, LessonSerializer
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """Serializer for user registration."""
+
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'email', 'password', 'password2',
+            'first_name', 'last_name', 'phone', 'city'
+        ]
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -48,8 +82,6 @@ class PaymentDetailSerializer(serializers.ModelSerializer):
     """Serializer for Payment model with detailed information."""
 
     user = UserSerializer(read_only=True)
-    course = CourseSerializer(read_only=True)
-    lesson = LessonSerializer(read_only=True)
 
     class Meta:
         model = Payment
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, PaymentViewSet
+from .views import UserViewSet, PaymentViewSet, UserRegistrationAPIView, UserLogoutAPIView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -8,4 +8,6 @@ router.register(r'payments', PaymentViewSet, basename='payment')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserRegistrationAPIView.as_view(), name='register'),
+    path('logout/', UserLogoutAPIView.as_view(), name='logout'),
 ]
